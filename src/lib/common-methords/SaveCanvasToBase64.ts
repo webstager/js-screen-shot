a96fa220e4067d29f4fe4940667f81e6b424f3aa
xiang.wang <xiang.wang@mihoyo.com>
2021-10-24 18:57:55 +0800

    feat:将图片放进剪贴板中

--- src/lib/common-methords/SaveCanvasToBase64.ts
+++ src/lib/common-methords/SaveCanvasToBase64.ts
@@ -6,7 +6,8 @@ export function saveCanvasToBase64(
   startX: number,
   startY: number,
   width: number,
-  height: number
+  height: number,
+  quality = 0.75
 ) {
   // 获取裁剪框区域图片信息
   const img = context.getImageData(startX, startY, width, height);
@@ -19,7 +20,37 @@ export function saveCanvasToBase64(
   if (imgContext) {
     // 将图片放进canvas中
     imgContext.putImageData(img, 0, 0);
+    // 将图片放进剪贴板中
+    canvas.toBlob(
+      (blob: any) => {
+        const clipboardItem = new (window as any).ClipboardItem({
+          [blob.type]: blob
+        });
+        (navigator.clipboard as any).write([clipboardItem]);
+        console.log("copy success!!!");
+      },
+      "image/png",
+      quality
+    );
+
     return canvas.toDataURL("png");
   }
   return "";
 }
+
+// clipboard demo
+// async function writeClipImg(imgURL: string) {
+//   try {
+//     const data = await fetch(imgURL);
+//     const blob = await data.blob();
+
+//     await navigator.clipboard.write([
+//       new ClipboardItem({
+//         [blob.type]: blob
+//       })
+//     ]);
+//     console.log('Fetched image copied.');
+//   } catch(err: any) {
+//     console.error(err.name, err.message);
+//   }
+// }
